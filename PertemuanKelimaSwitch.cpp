007766739cbeec833632062fc4f36673507c40e1
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-25 10:17:56 +0700

    Menambahkan Fungsi Penjumlahan

--- PertemuanKelimaSwitch.cpp
+++ PertemuanKelimaSwitch.cpp
@@ -31,8 +31,25 @@ int pembagian(int bil_1, int bil_2) {
     return bil_1 / bil_2;
 } 
 
-
 int main()
 {
+    int pilihan;
+
+    do {
+        system("cls");
+        cout << "=====Menu=====" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan menu yang diinginkan (1/2/3/4/5): ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+        }
+    }
     std::cout << "Hello World!\n";
 }
