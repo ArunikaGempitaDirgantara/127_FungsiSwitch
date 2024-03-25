91980704aaab4f4451d6b08565e1ac0585b2739d
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-25 10:11:30 +0700

    Menambahkan Fungsi Penjumlahan, Pengurangan, Perkalian, dan Pembagian

--- PertemuanKelimaSwitch.cpp
+++ PertemuanKelimaSwitch.cpp
@@ -15,6 +15,23 @@ void input()
     cin >> b;
 }
 
+int penjumlahan(int bil_1, int bil_2) {
+    return bil_1 + bil_2;
+}
+
+int pengurangan(int bil_1, int bil_2) {
+    return bil_1 - bil_2;
+}
+
+int perkalian(int bil_1, int bil_2) {
+    return bil_1 * bil_2;
+}
+
+int pembagian(int bil_1, int bil_2) {
+    return bil_1 / bil_2;
+} 
+
+
 int main()
 {
     std::cout << "Hello World!\n";
